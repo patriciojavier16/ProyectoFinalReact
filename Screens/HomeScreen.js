1a925656b11ced25ccf6fba4474b89613bbc81c3
Jacob Capa <jacobowiz.7@protonmail.com>
2023-08-02 21:04:02 -0500

    delete change

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,52 +1,7 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
 
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bienvenido a Mi App</Text>
-      <Text style={styles.subtitle}>Resumen de información relevante</Text>
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Ver Lista de Productos</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Ver Carrito de Compras</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  subtitle: {
-    fontSize: 18,
-    marginBottom: 30,
-  },
-  button: {
-    backgroundColor: 'blue',
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 8,
-    marginBottom: 10,
-    width: '100%',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+
